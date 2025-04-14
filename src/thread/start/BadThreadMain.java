f3465419ca5207b163ee1f66b6b86ec0f4ef0543
juhoon212 <ws5501@naver.com>
2025-04-14 21:40:42 +0900

    study: thread를 실행시키고 싶으면 start() 메소드를 실행해야한다.

--- src/thread/start/BadThreadMain.java
+++ src/thread/start/BadThreadMain.java
@@ -1,13 +1,12 @@
-import thread.start.HelloThread;
+package thread.start;
 
-public class Main {
+public class BadThreadMain {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName() + ": main() start!!");
 
         HelloThread helloThread = new HelloThread();
         System.out.println(Thread.currentThread().getName() + ": start() 호출 전");
-//        helloThread.start(); // main 스레드는 내부에 있는 run()을 호출하는것이 아닌 일만 이 스레드에 시키는것.
-
+        helloThread.run(); // run() 직접 실행
         System.out.println(Thread.currentThread().getName() + ": start() 호출 후");
 
         System.out.println(Thread.currentThread().getName() + ": main() end!!");
