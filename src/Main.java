c7e93120fdf2c650e896a35f87dcf248a3b81ccd
juhoon212 <ws5501@naver.com>
2025-04-14 21:22:52 +0900

    study: 스레드는 순서와 실행 기간을 모두 보장하지 않는다.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import thread.start.HelloThread;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Thread.currentThread().getName() + ": main() start!!");
+
+        HelloThread helloThread = new HelloThread();
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 전");
+        helloThread.start(); // main 스레드는 내부에 있는 run()을 호출하는것이 아닌 일만 이 스레드에 시키는것.
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 후");
+
+        System.out.println(Thread.currentThread().getName() + ": main() end!!");
     }
 }
