966723616a7db0891c8edfc7a1565929f55b2356
juhoon212 <ws5501@naver.com>
2025-04-26 22:42:30 +0900

    study: yield() 는 스레드의 실행 상태를 양보하는 것이다. 즉 cpu가 실행하고 있는 스레드의 상태를 스케쥴링 큐에 넣고 다른 스레드를 먼저 실행하게 만드는 것이다.

---
+++ src/thread/yield/YieldMain.java
@@ -0,0 +1,31 @@
+package thread.yield;
+
+import thread.start.HelloRunnable;
+
+import static util.ThreadUtils.sleep;
+
+public class YieldMain {
+
+    static final int THREAD_COUNT = 1000;
+
+    public static void main(String[] args) {
+        for (int i=0; i<THREAD_COUNT; ++i) {
+            Thread thread = new Thread(new MyRunnable());
+            thread.start();
+        }
+    }
+
+    static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i=0; i<10; ++i) {
+                System.out.println(Thread.currentThread().getName() + " - " + i);
+                // 1. empty()
+                // sleep(1) // 2.sleep
+                // Thread.yield() // 3.yield
+                Thread.yield(); // thread상태가 안바뀜 -> RUNNABLE(대기 큐에서 대기)
+            }
+        }
+    }
+}
