a8fb9baef32a01942d062bf96414c4aa6c720749
juhoon212 <ws5501@naver.com>
2025-06-03 22:09:08 +0900

    study: java에서 제공하는 대부분의 collection들은 thread-safe 하지 않다.

---
+++ src/thread/collection/simple/SimpleListManV2.java
@@ -0,0 +1,48 @@
+package thread.collection.simple;
+
+import thread.collection.simple.list.BasicList;
+import thread.collection.simple.list.SimpleList;
+
+import static util.MyLogger.log;
+
+public class SimpleListManV2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        test(new BasicList());
+    }
+
+    private static void test(SimpleList list) throws InterruptedException {
+        log(list.getClass().getSimpleName());
+
+        // A를 리스트에 저장하는 코드
+        final Runnable addA = new Runnable() {
+            @Override
+            public void run() {
+                list.add("A");
+                log("Thread-1: list.add(A)");
+            }
+        };
+
+        // A를 리스트에 저장하는 코드
+        final Runnable addB = new Runnable() {
+            @Override
+            public void run() {
+                list.add("B");
+                log("Thread-2: list.add(B)");
+            }
+        };
+
+        final Thread thread1 = new Thread(addA, "Thread-1");
+        final Thread thread2 = new Thread(addB, "Thread-2");
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        log(list);
+
+    }
+
+
+}
